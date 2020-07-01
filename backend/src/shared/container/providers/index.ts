f202ec115be6959752adaae42bfe85bedc2319d2
Vinicius David <vinicius.davidf@gmail.com>
2020-07-01 18:53:49 -0300

    amazon ses config

--- backend/src/shared/container/providers/index.ts
+++ backend/src/shared/container/providers/index.ts
@@ -1,25 +1,4 @@
-import { container } from 'tsyringe';
+import './StorageProvider';
+import './MailTemplateProvider';
+import './MailProvider';
 
-import IStorageProvider from '../providers/StorageProvider/models/IStorageProvider';
-import DiskStorageProvider from '../providers/StorageProvider/implementations/DiskStorageProvider';
-
-import IMailProvider from '../providers/MailProvider/models/IMailProvider';
-import EtherealMailProvider from '../providers/MailProvider/implementations/EtherealMailProvider';
-
-import IMailTemplateProvider from '../providers/MailTemplateProvider/models/IMailTemplateProvider';
-import HandlebarsMailTemplateProvider from '../providers/MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
-
-container.registerSingleton<IStorageProvider>(
-  'StorageProvider',
-  DiskStorageProvider
-);
-
-container.registerSingleton<IMailTemplateProvider>(
-  'MailTemplateProvider',
-  HandlebarsMailTemplateProvider
-);
-
-container.registerInstance<IMailProvider>(
-  'MailProvider',
-  container.resolve(EtherealMailProvider),
-);
