62d2fa50f4387a15122dcc040d2363c1e4ffd0c9
Vinicius David <vinicius.davidf@gmail.com>
2020-07-13 09:19:49 -0300

    appointment creatted page

--- appgobarber/src/pages/AppointmentCreated/index.tsx
+++ appgobarber/src/pages/AppointmentCreated/index.tsx
@@ -1,9 +1,52 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useCallback, useMemo } from 'react';
+import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import { format } from 'date-fns';
+import ptBr from 'date-fns/locale/pt-BR';
+
+import {
+  Container,
+  Title,
+  Description,
+  OkButton,
+  OkButtonText,
+} from './styles';
+
+interface RouteParams {
+  date: number;
+}
 
 const AppointmentCreated: React.FC = () => {
+  const { reset } = useNavigation();
+  const { params } = useRoute();
+
+  const routeParams = params as RouteParams;
+
+  const resetNavigate = useCallback(() => {
+    reset({
+      routes: [{ name: 'Dashboard' }],
+      index: 0,
+    });
+  }, [reset]);
+
+  const formattedDate = useMemo(() => {
+    return format(
+      routeParams.date,
+      "EEEE', dia' dd 'de' MMMM 'de' yyyy 'às' HH:mm'h'",
+      { locale: ptBr });
+  }, [routeParams.date]);
+
   return (
-    <View />
+    <Container>
+      <Icon name="check-circle" size={80} color="#04d361" />
+
+      <Title>Agendamento marcado!</Title>
+      <Description>{formattedDate}</Description>
+
+      <OkButton onPress={resetNavigate}>
+        <OkButtonText>Ok</OkButtonText>
+      </OkButton>
+    </Container>
   );
 };
 
---
+++ appgobarber/src/pages/AppointmentCreated/styles.ts
@@ -0,0 +1,39 @@
+import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
+
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  padding: 0 24px;
+`;
+
+export const Title = styled.Text`
+  font-size: 32px;
+  color: #f4ede8;
+  font-family: 'Ubuntu-Medium';
+  margin-top: 40px;
+  text-align: center;
+`;
+
+export const Description = styled.Text`
+  font-family: 'Ubuntu-Regular';
+  font-size: 18px;
+  color: #999591;
+  margin-top: 16px;
+`;
+
+export const OkButton = styled(RectButton)`
+  background: #ff9000;
+  justify-content: center;
+  align-items: center;
+  border-radius: 10px;
+  margin-top: 24px;
+  padding: 12px 24px;
+`;
+
+export const OkButtonText = styled.Text`
+  font-family: 'Ubuntu-Medium';
+  font-size: 18px;
+  color: #312e38;
+`;
